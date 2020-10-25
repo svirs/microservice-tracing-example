ae60bfdacca93228c2ac1f1b9916c364af87bc0f
svirs <1.singh.shur@gmail.com>
2020-10-24 22:30:21 -0700

    set up workspaces and basic backend

---
+++ workspaces/backend/src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (_: Request, res: Response) => {
+  res.status(200).send("Hello World!");
+});
+
+app.listen(8000, () => {
+  console.log('Server Started at Port, 8000');
+});

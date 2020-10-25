e183453b8855e46346dd0b7f47c7f71f7897a74e
svirs <1.singh.shur@gmail.com>
2020-10-25 10:34:54 -0700

    initial microservice children impl

--- workspaces/backend/src/index.ts
+++ workspaces/backend/src/index.ts
@@ -1,9 +1,34 @@
 import express, { Request, Response } from "express";
+import { spawn, ChildProcess } from 'child_process';
 
 const app = express();
 
-app.get("/", (_: Request, res: Response) => {
-  res.status(200).send("Hello World!");
+const children: ChildProcess[] = []
+
+app.get("/create", async (req: Request, res: Response) => {
+  children.forEach(childProcess => childProcess?.kill());
+  const setup = req.query.setup ?? '{"root":[]}'
+  // { root: ['node1', 'node2'], node1: [node: 3], node2: [], node3: []}
+  const nodeMap: {[k: string]: any} = {};
+  let port = 4000;
+  // name, port, workTime, ...targets
+  for (const [nodeID, targets] of Object.entries(JSON.parse(setup as string))) {
+    if (nodeID === 'root') continue;
+    nodeMap[nodeID as string] = {
+      name: nodeID,
+      port,
+      workTime: 2500,
+      targets: '[]'
+    }
+    const c = spawn('yarn', ['start', nodeID, `${port}`, "2500", '[]'], {
+      cwd: '../microservice',
+    });
+    
+    c.stdout.on('data', (data) => console.log(data?.toString()))
+    children.push(c)
+    port += 1;
+  }
+  res.status(200).send("huh");
 });
 
 app.listen(8000, () => {
--- workspaces/microservice/src/index.ts
+++ workspaces/microservice/src/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
+import { get } from 'http';
 
-const [name, port, workTime] = process.argv.slice(2);
+console.log(process.argv);
+const [name, port, workTime, targets] = process.argv.slice(2);
+const targetsArray = JSON.parse(targets ?? '[]' as unknown as string);
 const workMS = parseInt(workTime, 10);
 
 const app = express();
@@ -14,11 +17,25 @@ app.get("/", async (req: Request, res: Response) => {
   console.log('Finished work', {
     name, port, time: Date.now(), traceId,
   })
-  res.status(200).send("Hello World!");
+  let data = [];
+  data = await Promise.all(targetsArray.map((url: string) => new Promise( resolve => get(url, response => {
+    var body = '';
+    response.on('data', function(chunk) {
+      body += chunk;
+    });
+    response.on('end', function() {
+      resolve(body);
+    });
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  }))));
+  res.status(200).send(data);
 });
 
-app.listen(8000, () => {
-  console.log('Server Started at Port 8000', {
+app.get("/asdf", async (req, res) => { res.status(200).send("asdf") })
+
+app.listen(port, () => {
+  console.log(`Server Started at Port ${port}`, {
     name, port, time: Date.now()
   });
 });
@@ -26,3 +43,5 @@ app.listen(8000, () => {
 function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+export default app;
